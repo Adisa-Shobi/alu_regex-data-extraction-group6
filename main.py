60ddf03fe4f418d8874c51cb018a298d2d657c3a
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-10-03 15:40:21 +0000

    Update: three data type patterns done

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import re
+import sys
+
+restaurant_pattern='''@Oden'''
+ingredient_pattern='''@Glen'''
+RGB_pattern='''rgb\(\d{1,3},\s*\d{1,3},\s*\d{1,3}\)'''
+social_pattern='''@meru'''
+product_pattern='''@Ade'''
+news_pattern='''@Glen'''
+event_pattern=r'''\w{3}\s\d{2},\s\d{4}\s-\s\d{2}:\d{2}\s(AM|PM)'''
+email_pattern='''[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'''
+
+def get_lines(file_name: str) -> [str]:
+    """
+    This function returns the lines from the file as a list.
+    It handles the opening and closing of the file.
+    Also, the function assumes the file exists and can be read.
+    """
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    return lines
+
+data = get_lines(sys.argv[1])
+colors = []
+events = []
+emails = []
+for item in data:
+    colors += re.findall(RGB_pattern, item)
+    events += re.findall(event_pattern, item)
+    emails += re.findall(email_pattern, item)
+print(colors)
+print(events)
+print(emails)
